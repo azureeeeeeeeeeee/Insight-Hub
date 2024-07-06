ef16083cca864ef7c871cf1b840939dfadeea92d
rionando <rionandosoeksin@gmail.com>
2024-07-07 00:44:19 +0800

    adding stats in dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,58 +1,42 @@
-import {
-  Avatar,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  ButtonGroup,
-  Button,
-} from "@chakra-ui/react";
+import { Avatar, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { FaRegUser, FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-16 py-4">
-      <div className="flex-none">
-        <h1>
-          <Link to={"/"}>
-            <Avatar name="logo" src="LOGO.png" />
-          </Link>
-        </h1>
-      </div>
-      <div className="flex-none">
-        <Menu>
-          <MenuButton>
-            <Avatar name="username" src="https://bit.ly/dan-abramov" />
-          </MenuButton>
-          <MenuList>
-            <Link to="/profile">
-              <MenuItem>
-                <FaRegUser className="mr-4" />
-                Settings
-              </MenuItem>
+    <>
+      <nav className="flex items-center justify-between px-16 py-4">
+        <div className="flex-none">
+          <h1>
+            <Link to={"/"}>
+              <Avatar name="logo" src="LOGO.png" />
             </Link>
-            <Link>
-              <MenuItem>
-                <FaSignOutAlt className="mr-4" />
-                Logout
-              </MenuItem>
-            </Link>
-          </MenuList>
-        </Menu>
-        {/* If not logged in */}
-        {/* <ButtonGroup>
-            <Link to="/register">
-              <Button colorScheme="blue" variant="outline">
-                REGISTER
-              </Button>
-            </Link>
-            <Link to="/login">
-              <Button colorScheme="blue">LOGIN</Button>
-            </Link>
-          </ButtonGroup> */}
-      </div>
-    </nav>
+          </h1>
+        </div>
+        <div className="flex-none">
+          <Menu>
+            <MenuButton>
+              <Avatar name="username" src="https://bit.ly/dan-abramov" />
+            </MenuButton>
+            <MenuList>
+              <Link to="/profile">
+                <MenuItem>
+                  <FaRegUser className="mr-4" />
+                  Settings
+                </MenuItem>
+              </Link>
+              <Link>
+                <MenuItem>
+                  <FaSignOutAlt className="mr-4" />
+                  Logout
+                </MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+        </div>
+      </nav>
+      <hr />
+    </>
   );
 };
 
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,7 +1,48 @@
 // import React from 'react'
+import { Heading, Text, Button, ButtonGroup, Box } from "@chakra-ui/react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <>
+      <Heading mt={8} size="xl">
+        Hi @User
+      </Heading>
+
+      <Box mt={8}>
+        <Heading size="lg">Recent Activity</Heading>
+        <Box mt={2}>
+          <Text>
+            <strong>Analyzed</strong> : sales_data.csv - 2 hours ago
+          </Text>
+          <Text>
+            <strong>Generated Report</strong> : Q2 Analysis - 1 day ago
+          </Text>
+          <Text>
+            <strong>Shared Dataset</strong> : marketing_data.csv - 3 days ago
+          </Text>
+        </Box>
+      </Box>
+
+      <Box mt={8}>
+        <Heading size="lg">Your Progress</Heading>
+        <Box mt={2}>
+          <Text>
+            <strong>Datasets Analyzed</strong> : 5/10
+          </Text>
+          <Text>
+            <strong>Reports Created</strong> : 3/5
+          </Text>
+        </Box>
+      </Box>
+
+      <Text mt={8}>What do you want to do today ?</Text>
+      <ButtonGroup mt={4}>
+        <Button colorScheme="blue">Add new Data</Button>
+        <Button colorScheme="yellow">Analyze your data</Button>
+        <Button colorScheme="green">Create a report</Button>
+      </ButtonGroup>
+    </>
+  );
 };
 
 export default DashboardPage;
